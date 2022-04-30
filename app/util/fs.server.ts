a8a4384d98034f97e7f67be86c1024f3a5726603
David Crespo <crespo.dm@gmail.com>
2022-04-29 22:02:38 -0500

    try without ..

--- app/util/fs.server.ts
+++ app/util/fs.server.ts
@@ -4,7 +4,7 @@ import path from "path";
 const CONTENT = "content";
 
 export const readContentDir = async () =>
-  fs.readdir(path.join(__dirname, "..", CONTENT));
+  fs.readdir(path.join(__dirname, CONTENT));
 
 export const readContentFile = async (file: string) =>
-  fs.readFile(path.join(__dirname, "..", CONTENT, file), "utf-8");
+  fs.readFile(path.join(__dirname, CONTENT, file), "utf-8");
