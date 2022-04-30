8a94019923c35e4f3d28725b883cce9c51e134e4
David Crespo <crespo.dm@gmail.com>
2022-04-29 22:05:58 -0500

    now we're logging

--- app/util/fs.server.ts
+++ app/util/fs.server.ts
@@ -3,8 +3,13 @@ import path from "path";
 
 const CONTENT = "content";
 
-export const readContentDir = async () =>
-  fs.readdir(path.join(__dirname, CONTENT));
+export const readContentDir = async () => {
+  console.log(await fs.readdir(__dirname));
+  console.log(await fs.readdir(path.join(__dirname, "..")));
+  console.log(await fs.readdir(path.join(__dirname, "../..")));
+  console.log(await fs.readdir(path.join(__dirname, "../../..")));
+  return fs.readdir(path.join(__dirname, CONTENT));
+};
 
 export const readContentFile = async (file: string) =>
   fs.readFile(path.join(__dirname, CONTENT, file), "utf-8");
